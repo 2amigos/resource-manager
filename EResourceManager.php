2c1b4675e9aa6459872f764f28d04d0a064ff49b
Alexander Kochetov <creocoder@gmail.com>
2013-12-25 13:43:10 +0400

    EResourceManager class added

---
+++ EResourceManager.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @link http://github.com/2amigos/resource-manager
+ * @copyright Copyright (c) 2013 2amigOS! Consulting Group, LLC
+ * @license http://opensource.org/licenses/BSD-3-Clause
+ */
+
+/**
+ * EResourceManager class.
+ *
+ * @author Alexander Kochetov <creocoder@gmail.com>
+ * @author Timur Ruziev <resurtm@gmail.com>
+ */
+abstract class EResourceManager extends CApplicationComponent
+{
+	/**
+	 * @param CUploadedFile $file
+	 * @param string $name
+	 * @param array $options
+	 */
+	abstract public function saveFile($file, $name, $options = array());
+
+	/**
+	 * @param string $name
+	 * @return string
+	 */
+	abstract public function getFileUrl($name);
+
+	/**
+	 * @param string $name
+	 * @return boolean
+	 */
+	abstract public function getIsFileExist($name);
+}
